122a580e5f9f07832285545adfbffd1951e92de2
Salianashruth <Salianashruth@gmail.com>
2025-01-02 23:35:23 +0530

    Change 1

--- Helloworld.java
+++ Helloworld.java
@@ -1,20 +1,7 @@
 // HelloWorld.java
 public class HelloWorld {
-
     public static void main(String[] args) {
-        // Print a simple message
+        // Print "Hello, World!" to the console
         System.out.println("Hello, World!");
-
-        // Example of a switch expression introduced in Java 17
-        String day = "MONDAY";
-
-        String typeOfDay = switch (day) {
-            case "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" -> "Weekday";
-            case "SATURDAY", "SUNDAY" -> "Weekend";
-            default -> throw new IllegalArgumentException("Invalid day: " + day);
-        };
-
-        // Print the result
-        System.out.println(day + " is a " + typeOfDay + ".");
     }
 }
